5b2d7ff0430161ac526ce16d389266107eed4bf0
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2020-07-01 22:37:02 +0200

    created double loop for board

--- Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Program.cs
@@ -6,7 +6,20 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            char[,] board = new char[3, 3];
+            board[0, 2] = 'X';
+
+            for(int row = 0; row < 3; row++)
+            {
+                Console.WriteLine();
+                for( int col = 0; col < 3; col++)
+                {
+                    
+                    Console.Write(board[row, col]+" | ");
+                }
+                
+            }
         }
     }
 }
