742dc203eef482d1a5b9ffe9ac2d90006cfe92c7
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2020-07-02 08:19:59 +0200

    fixed loop

--- Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Program.cs
@@ -8,17 +8,30 @@ namespace Tic_Tac_Toe
         {
 
             char[,] board = new char[3, 3];
+            
+
+
+            for (int row = 0; row < 3; row++)
+            {
+                
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = ' ';
+                }
+
+            }
+
             board[0, 2] = 'X';
 
-            for(int row = 0; row < 3; row++)
+            for (int row = 0; row < 3; row++)
             {
-                Console.WriteLine();
+                Console.Write("| ");
                 for( int col = 0; col < 3; col++)
                 {
+                    Console.Write(board[row, col]+ " | ");
                     
-                    Console.Write(board[row, col]+" | ");
                 }
-                
+                Console.WriteLine();
             }
         }
     }
